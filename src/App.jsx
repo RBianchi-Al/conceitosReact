cb07f794842bf8703b561a81bd3892b8dbb15fc7
RBianchi-Al <renatalebianchi@gmail.com>
2021-07-01 11:59:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/global.scss';
+import {RepositoryList} from '../src/components/RepositoryList'
+
+export function App() {
+     return( 
+        <div>
+             <RepositoryList/>
+        </div>
+    
+     
+     )
+  
+}
